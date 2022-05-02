49a7dee25402195ff43a63af5ae3bd87892272b0
myTerminal <team.terminal@gmail.com>
2022-05-02 11:27:18 -0500

    add initial implementation

---
+++ index.js
@@ -0,0 +1,33 @@
+/* global module require */
+
+const express = require('express');
+const axios = require('axios');
+
+const { basePath, aliases } = require('./config.json');
+
+module.exports = url => {
+    const app = express();
+
+    app.listen(
+        url,
+        () => {
+            console.log('GitHub proxy started on', url);
+        }
+    );
+
+    app.get(
+        '*',
+        ({ path }, res) => {
+            const requestPath = aliases[path] || path;
+
+            axios
+                .get(`https://raw.githubusercontent.com/${basePath}${requestPath}`)
+                .then(
+                    ({ data }) => {
+                        res.contentType('text/plain');
+                        res.send(data);
+                    }
+                );
+        }
+    );
+};
